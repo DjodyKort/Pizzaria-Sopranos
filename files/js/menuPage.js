d7f2cbf0a97f8614ceb96514977bf08daa0e25ee
BestUserNameYouWillSee <77840705+BestUserNameYouWillSee@users.noreply.github.com>
2023-12-18 14:51:45 +0100

    I need help

--- files/js/menuPage.js
+++ files/js/menuPage.js
@@ -1,20 +1,96 @@
-var MenuCheckbox = document.querySelectorAll('input[type="checkbox"]');
+const Pizzas = [
+  "Pizza Salami",
+  "Pizza Margherita",
+  "Pizza Funghi",
+  "Pizza Calzone",
+  "Pizza Prosciutto",
+  "Pizza Quattro Formaggi",
+  "Pizza Verdure",
+  "Pizza Marinara",
+];
+const PizzaPrices = [
+  "€11,50",
+  "€9,99",
+  "€12,99",
+  "€13,50",
+  "€13,50",
+  "€12,50",
+  "€10,50",
+  "€11,99",
+];
 
-MenuCheckbox.forEach(function (checkbox) {
-  checkbox.addEventListener("change", function () {
-    getCheckboxValues();
-  });
-});
+for (let i = 0; i < Pizzas.length; i++) {
+  var menuItemId = "MenuItem" + i;
+  createElement(menuItemId);
+  function createElement(menuItemId) {
 
-function getCheckboxValues() {
-  var checkedCheckboxes = document.querySelectorAll(
-    'input[type="checkbox"]:checked'
-  );
-  var values = [];
-  checkedCheckboxes.forEach(function (checkbox) {
-    values.push(checkbox.value);
-  });
-  document.getElementById("CartTextField").innerHTML = values.join(", <br>");
 
-  console.log("Checkbox values: " + values.join(", "));
+    const MenuContainer = document.getElementById("MenuContainer");
+    const MenuItem = document.createElement("div");
+    MenuItem.id = menuItemId;
+    MenuContainer.appendChild(MenuItem);
+
+    const PizzaImg = document.createElement("img");
+    PizzaImg.src = "../../images/pizza.png";
+    PizzaImg.alt = Pizzas[i];
+    PizzaImg.className = "PizzaImg";
+    MenuItem.appendChild(PizzaImg);
+
+    const MenuPizzaName = document.createElement("h1");
+    MenuPizzaName.className = "MenuText Pizza";
+    MenuPizzaName.textContent = Pizzas[i];
+    MenuItem.appendChild(MenuPizzaName);
+
+    const MenuPizzaPrice = document.createElement("h1");
+    MenuPizzaPrice.className = "MenuText Prijs";
+    MenuPizzaPrice.textContent = PizzaPrices[i];
+    MenuItem.appendChild(MenuPizzaPrice);
+
+    const MenuInputs = document.createElement("div");
+    MenuInputs.id = "MenuInputs";
+    MenuItem.appendChild(MenuInputs);
+
+    const MenuInputText = document.createElement("input");
+    MenuInputText.id = "MenuItem" + i + "Input";
+    MenuInputText.className = "MenuInputText";
+    MenuInputText.type = "text";
+    MenuInputText.value = "0";
+    MenuInputs.appendChild(MenuInputText);
+
+    const MenuButton = document.createElement("input");
+    MenuButton.addEventListener("click", function () {
+      AddCart(menuItemId);
+    });
+    MenuButton.className = "MenuButton";
+    MenuButton.type = "button";
+    MenuButton.value = "add";
+    MenuInputs.appendChild(MenuButton);
+  }
+}
+
+function AddCart(e) {
+  var ItemId = document.getElementById(e);
+  if (e != null) {
+    var h1Element = ItemId.querySelector(".Pizza");
+    var h1Prijs = ItemId.querySelector(".Prijs");
+    var PizzaName = h1Element.textContent;
+    var PizzaPrice = parseFloat(
+      h1Prijs.textContent.replace("€", "").replace(/,/g, ".")
+    );
+    var PizzaAmount = document.getElementById(e + "Input");
+    var PizzaAmountText = parseFloat(PizzaAmount.value);
+
+    var PizzaPriceTotal = 0;
+
+    PizzaPriceTotal = (PizzaPrice * PizzaAmountText).toFixed(2);
+
+    var PizzaPriceTotalString = String(PizzaPriceTotal).replace(".", ",");
+    console.log(PizzaPriceTotal);
+    document.getElementById("CartTextField").textContent = PizzaName;
+    document.getElementById("CartTextAmount").textContent = PizzaAmountText;
+    document.getElementById("CartPrice").textContent =
+      "€" + PizzaPriceTotalString;
+  } else {
+    console.log("error het werkt niet");
+  }
 }
--- files/php/pages/menu.php
+++ files/php/pages/menu.php
@@ -9,63 +9,12 @@
 </head>
 
 <body class="MenuBody">
-    <section class="MenuContainer">
-        <div class="MenuItem1">
-            <input type="checkbox" class="invisible" name="MenuItem1" id="MenuItem1" value="Pizza Salami">
-            <label for="MenuItem1" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Salami">
-                <h1 class="MenuText">Pizza Salami</h1>
-            </label>
-        </div>
-        <div class="MenuItem2">
-            <input type="checkbox" class="invisible" name="MenuItem2" id="MenuItem2" value="Pizza Margherita">
-            <label for="MenuItem2" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Margherita">
-                <h1 class="MenuText">Pizza Margherita</h1>
-            </label>
-        </div>
-        <div class="MenuItem3">
-            <input type="checkbox" class="invisible" name="MenuItem3" id="MenuItem3" value="Pizza Funghi">
-            <label for="MenuItem3" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Funghi">
-                <h1 class="MenuText">Pizza Funghi</h1>
-            </label>
-        </div>
-        <div class="MenuItem4">
-            <input type="checkbox" class="invisible" name="MenuItem4" id="MenuItem4" value="Pizza Calzone">
-            <label for="MenuItem4" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Calzone">
-                <h1 class="MenuText">Pizza Calzone</h1>
-            </label>
-        </div>
-        <div class="MenuItem5">
-            <input type="checkbox" class="invisible" name="MenuItem5" id="MenuItem5" value="Pizza Prosciutto">
-            <label for="MenuItem5" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Prosciutto">
-                <h1 class="MenuText">Pizza Prosciutto</h1>
-            </label>
-        </div>
-        <div class="MenuItem6">
-            <input type="checkbox" class="invisible" name="MenuItem6" id="MenuItem6" value="Pizza Quattro Formaggi">
-            <label for="MenuItem6" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Quattro Formaggi">
-                <h1 class="MenuText">Pizza Quattro Formaggi</h1>
-            </label>
-        </div>
-        <div class="MenuItem7">
-            <input type="checkbox" class="invisible" name="MenuItem7" id="MenuItem7" value="Pizza Verdure">
-            <label for="MenuItem7" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Verdure">
-                <h1 class="MenuText">Pizza Verdure</h1>
-            </label>
-        </div>
-        <div class="MenuItem8">
-            <input type="checkbox" class="invisible" name="MenuItem8" id="MenuItem8" value="Pizza Marinara">
-            <label for="MenuItem8" class="MenuLabel">
-                <img class="PizzaImg" src="../../images/pizza.png" alt="Pizza Marinara">
-                <h1 class="MenuText">Pizza Marinara</h1>
-            </label>
-        </div>
+    <section class="MenuContainer" id="MenuContainer">
+        
+
+
+
+
     </section>
 
     <section class="CartContainer">
@@ -74,6 +23,8 @@
                 <h1 class="MenuText CartText">Uw winkelwagen</h1>
                 <div class="OrderContainer">
                     <h1 class="MenuText CartOrder" id="CartTextField"></h1>
+                    <h1 class="MenuText CartOrder" id="CartTextAmount"></h1>
+                    <h1 class="MenuText CartOrder" id="CartPrice"></h1>
                 </div>
             </div>
             <button type="submit" class="CartConfirm">Bestellen</button>

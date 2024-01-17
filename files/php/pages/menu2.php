efc133f8104c0e9b16642bd8e7f2e3f9ffd6dbd1
Dinand Nafzger <bosjesman2005@gmail.com>
2024-01-17 10:39:52 +0100

    menu2 topping's fixed
    Made a dynamic system for ordering pizza's and putting the desired toppings on the pizza

--- files/php/pages/menu2.php
+++ files/php/pages/menu2.php
@@ -6,41 +6,54 @@ require_once('../functions.php');
 require_once('../classes.php');
 
 // ============ Declaring Variables ============
-$arrayToppings = array('tomat', 'chez', 'beef', 'chez but more');
+$tableDishes = 'dishes';
+$tableDefaultToppings = 'defaultToppingRelations';
+$tableToppings = 'toppings';
 
 // ============ Start of Program ============
 # Header
+ob_start();
 Functions::htmlHeader(320);
+//sql statement for menu1 
+
+$result = PizzariaSopranosDB::pdoSqlReturnArray("SELECT * FROM $tableDishes");
 
 // Switchers between menu1 and menu2
 if (isset($_POST['menu1'])) {
-    $pizza = $_POST['pizzaType'];
+    $pizzaID = $_POST['pizzaID'];
     if ($_SESSION['boolPreventHeader']) {
-        header("Location: ./menu2.php?pizza=$pizza");
+        header("Location: ./menu2.php?pizzaID=$pizzaID");
         $_SESSION['boolPreventHeader'] = false;
     }
 }
 
-if (!isset($_GET['pizza'])) {
+if (!isset($_GET['pizzaID'])) {
     //here needs to come a foreach loop for when all pizza's are added to the database
-    echo (" 
-    <img src='../../images/pizza.png' height='320px' alt='Responsive image' value='Quatre Formagi'><br/>
-    quatre formagi
-    <form method='post'>
-        <input type='submit' name='menu1' value='submit'>
-        <input type='hidden' name='pizzaType' value='quatre formagi'>
-    </form>
-");
-
+    foreach($result as $row){
+        echo($_SESSION['total']);
+        echo (" 
+        <form method='post'>
+            <input type='submit' name='menu1' value='Bestellen'>
+            <input type='hidden' name='pizzaID' value='".$row['dishID']."'>
+        </form>
+        ");
+    }
+    
     $_SESSION['boolPreventHeader'] = true;
-
 } else {
+    //Get value
+    $pizzaID = $_GET['pizzaID'];
+    $arrayToppings = PizzariaSopranosDB::pdoSqlReturnArray("SELECT t.name , t.price
+        FROM $tableDefaultToppings dt
+        JOIN $tableToppings t ON dt.toppingID = t.toppingID
+        WHERE dt.dishID = $pizzaID
+    ");
     echo ("
-    <form method='post'>
     <p>Pizza Bottom</p>
-        <select>
-            <option value='0'>Small</option>
-            <option value='1'>Medium</option>
+        <select id='sizeSelector' onchange='updatePrice()'>
+            <option value='0'>Normaal</option>
+            <option value='2'>Groot + €2</option>
+            <option value='4'>XXL + €4</option>
         </select>
         <select>
             <option value='0'>tomato</option>
@@ -52,118 +65,176 @@ if (!isset($_GET['pizza'])) {
         echo ("
             <tr>
                 <td>
-                    <img src='../../images/minus.png' height='25px' alt='reduction' style='cursor: pointer;' onclick='reduction($i)'>
+                    <img src='../../images/minus.png' height='25px' alt='reduction' style='cursor: pointer;' onclick='reduction(`".$arrayToppings[$i]['name'] .'`,'.$arrayToppings[$i]['price']." )'>
                 </td>
                 <td>
-                    <p style='margin: 5px 0 5px 15px;'>" . $arrayToppings[$i] . "</p> 
+                    <p style='margin: 5px 0 5px 15px;'>" . $arrayToppings[$i]['name'] . "</p> 
                 </td>
                 <td>
-                    <p id='counter$i' style='margin: 5px 15px;'>1</p>
+                    <p id='counter".$arrayToppings[$i]['name']."' style='margin: 5px 15px;'>1</p>
+                    <input type='hidden' id='".$arrayToppings[$i]['name']."' value='true'>
                 </td>
                 <td>
-                    <img src='../../images/plus.png' height='25px' alt='Increment' style='cursor: pointer;' onclick='increment($i)'>
+                    <img src='../../images/plus.png' height='25px' alt='Increment' style='cursor: pointer;' onclick='increment(`".$arrayToppings[$i]['name'] .'`,'.$arrayToppings[$i]['price'].")'>
                 <td>
             </tr>");
     }
 
     // Add a row for adding a new topping
+    
+    $arrMoney = PizzariaSopranosDB::pdoSqlReturnArray("SELECT `price` FROM $tableDishes WHERE `dishID` = $pizzaID");
+    $money = $arrMoney[0]['price'];
+
+    $arrToppings = PizzariaSopranosDB::pdoSqlReturnArray("SELECT * FROM $tableToppings ORDER BY `name` ASC");
+
     echo ("</table>
-    <table>
-        <tr>
-            <td>
-                pepperoni
-            </td>
-            <td colspan='4'>
-                <img src='../../images/plus.png' height='25px' alt='Add Topping' style='cursor: pointer;' onclick='addNewTopping(`pepperoni`)'>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                bacon
-            </td>
-            <td colspan='4'>
-                <img src='../../images/plus.png' height='25px' alt='Add Topping' style='cursor: pointer;' onclick='addNewTopping(`bacon`)'>
-            </td>
-        </tr>
+    <table> ");
+    foreach ($arrToppings as $index => $topping) {
+        if ($index < 3) {
+            // Display the first 3 toppings directly
+            echo("
+                <tr>
+                    <td>
+                    ".$topping['name']."
+                    </td>
+                    <td colspan='4'>
+                        <img src='../../images/plus.png' height='25px' alt='Add Topping' style='cursor: pointer;' onclick='addNewTopping(`".$topping['name']."` , ".$topping['price'].")'>
+                    </td>
+                </tr>
+            ");
+        } else {
+            // Display a dropdown for additional toppings
+            echo("
+                <tr class='additional-toppings' style='display: none;'>
+                    <td>
+                        ".$topping['name']."
+                    </td>
+                    <td colspan='4'>
+                        <img src='../../images/plus.png' height='25px' alt='Add Topping' style='cursor: pointer;' onclick='addNewTopping(`".$topping['name']."` , ".$topping['price'].")'>
+                    </td>
+                </tr>
+            ");
+        }
+    }
+        echo("
+    <tr id='toggleRow'>
+        <td colspan='5'>
+            <button onclick='toggleAdditionalToppings()'>Show More Toppings</button>
+        </td>
+    </tr>
     </table>
-    <input type='submit'>
+    <form method='post'>
+        <input type='submit' name='submitPizza' id='submitButton' value='Add to Cart Amount €".$money."' onclick='updateHidden();'>
+        <input type='hidden' id='money' name='money' value='".floatval($money)."' />
     </form>
     ");
+
+    if(isset($_POST['submitPizza'])){
+        $_SESSION['total'] = $_POST['money'];
+        header("Location: menu2.php");
+    }
 }
 
 # Footer
 Functions::htmlFooter();
+ob_end_flush();
 ?>
 
 <script>
+    //chnage from int to long
+    var total = parseFloat(document.getElementById("money").value);
     // Function to increment the counter
-    function increment(i, topping) {
-        if (!topping) {
-            var parameter = i;
-        } else {
-            var parameter = topping;
-        }
-        var currentCounter = parseInt(document.getElementById('counter' + parameter).innerText);
+    function increment(topping , toppingPrice) {
+        var currentCounter = parseInt(document.getElementById('counter' + topping).innerText);
+           
         if (currentCounter < 3) {
             var newCounter = currentCounter + 1;
-            document.getElementById('counter' + parameter).innerText = newCounter;
-
+            document.getElementById('counter' + topping).innerText = newCounter;
+            total += toppingPrice;
+            var roundedNumber = parseFloat(total).toFixed(2);
+            document.getElementById("submitButton").value = 'Add to Cart Amount €' + roundedNumber;
         }
     }
 
     // Function to add a new topping row
-    function addNewTopping(topping) {
+    function addNewTopping(topping, toppingPrice) {
         var table = document.querySelector('table');
-
-        // Check if the counter element exists
         var counterElement = document.getElementById('counter' + topping);
-
-        // If it doesn't exist, create it with an initial value of 1
         if (!counterElement) {
             var newRow = document.createElement('tr');
             newRow.innerHTML = `
             <td>
-                <img src='../../images/minus.png' height='25px' alt='reduction' style='cursor: pointer;' onclick='reduction("${topping}")'>
+                <img src='../../images/minus.png' height='25px' alt='reduction' style='cursor: pointer;' onclick='reduction("${topping}" , ${toppingPrice})'>
             </td>
             <td>
                 <p style='margin: 5px 0 5px 15px;'>${topping}</p> 
             </td>
             <td>
-                <p id='counter${topping}' style='margin: 5px 15px;'>1</p>
+                <p id='counter${topping}' style='margin: 5px 15px;'>0</p>
+                <input type='hidden' id='${topping}' value='false'>
             </td>
             <td>
-                <img src='../../images/plus.png' height='25px' alt='Increment' style='cursor: pointer;' onclick='increment("${topping}")'>
+                <img src='../../images/plus.png' height='25px' alt='Increment' style='cursor: pointer;' onclick='increment("${topping}" , ${toppingPrice})'>
             </td>`;
             table.appendChild(newRow);
-        } else if (counterElement) {
-
-
+            increment(topping, toppingPrice)
+        } else if (counterElement) {    
             var currentCounterElement = document.getElementById('counter' + topping);
-            console.log(currentCounterElement);
             var currentCounter = parseInt(currentCounterElement.innerText);
-            increment(currentCounter, topping);
+            increment(topping, toppingPrice);
         }
     }
 
 
-
-    function reduction(i) {
-        // Get the current value of the counter
-        var currentCounterElement = document.getElementById('counter' + i);
+    //function for reducing the amount of an counter
+    function reduction(topping , toppingPrice) {
+        var currentCounterElement = document.getElementById('counter' + topping);
         var currentCounter = parseInt(currentCounterElement.innerText);
-
+        var inputHidden = document.getElementById(topping).value; 
+        console.log(typeof inputHidden);
         if (currentCounter > 0) {
-            // Update the counter
             var newCounter = currentCounter - 1;
             currentCounterElement.innerText = newCounter;
-
-            // Check if the counter is now 0, and if so, remove the row
-            console.log(i);
-            if (newCounter === 0 && typeof i === 'string') {
+            total -= toppingPrice;
+            var roundedNumber = parseFloat(total).toFixed(2);
+            document.getElementById("submitButton").value = 'Add to Cart Amount €' + roundedNumber;
+            if (newCounter === 0 && inputHidden == 'false') {
                 var rowToRemove = currentCounterElement.parentNode.parentNode;
                 rowToRemove.parentNode.removeChild(rowToRemove);
             }
         }
     }
 
+    var sizeSelector = document.getElementById('sizeSelector');
+    var previousSizeValue = sizeSelector.options[sizeSelector.selectedIndex].value;
+
+     function updatePrice() {
+        var selectedValue = sizeSelector.options[sizeSelector.selectedIndex].value;
+        console.log(total);
+        // Subtract the cost of the previous size
+        total -= parseFloat(previousSizeValue);
+
+        // Add the cost of the new size
+        total += parseFloat(selectedValue);
+
+        // Update the displayed total
+        var roundedNumber = parseFloat(total).toFixed(2);
+        
+        document.getElementById("submitButton").value = 'Add to Cart Amount €' + roundedNumber;
+
+        // Update the previous size value for the next change
+        previousSizeValue = selectedValue;
+    }
+
+    function toggleAdditionalToppings() {
+        var additionalToppings = document.getElementsByClassName('additional-toppings');
+
+        for (var i = 0; i < additionalToppings.length; i++) {
+            additionalToppings[i].style.display = (additionalToppings[i].style.display === 'none') ? 'table-row' : 'none';
+        }
+    }
+
+    function updateHidden(){
+        document.getElementById("money").value = parseFloat(total).toFixed(2); 
+    }
 </script>
